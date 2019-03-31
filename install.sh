dfd98ea618b1c415ea2b2b95855d05b34b72770f
Pedro Lima <phgdl.19@gmail.com>
2019-03-31 20:13:30 -0300

    DB Ip fix

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ cp ${project_path}/magento2/app/etc/env.php.dev ${project_path}/magento2/app/etc
 
 cd ${project_path}/docker-magento2;
 echo "${color_green}Populating DB${color_empty}";
-docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html/mysql_dumps/; mysql -u magento -pmagento -h 172.18.0.3 magento < magento.sql";
+docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html/mysql_dumps/; mysql -u magento -pmagento -h docker-magento2_db_1 magento < magento.sql";
 echo "${color_green}Installing missing php extension${color_empty}";
 docker exec -ti docker-magento2_web_1 sh -c "docker-php-ext-install bcmath;";
 echo "${color_green}Restarting apache${color_empty}";
@@ -59,19 +59,19 @@ echo "${color_green}Fixing Permissions${color_empty}";
 docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; set-permissions";
 
 echo "${color_green}Add docker apache IP to hosts${color_empty}";
-sudo -- sh -c "echo '172.18.0.3 local.magento.com' >> /etc/hosts";
+sudo -- sh -c "echo 'docker-magento2_web_1 local.magento.com' >> /etc/hosts";
 
-echo "${color_azul}========================================Warning=======================================${color_empty}";
-echo "${color_azul}If you get any SQL error, the reason should be wrong MYSQL docker IP, the right IP is:${color_empty}";
-docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' docker-magento2_db_1;
-echo "${color_azul}Run the next command with the correct IP:${color_empty}";
-echo 'docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html/mysql_dumps/; mysql -u magento -pmagento -h 172.18.0.3 magento < magento.sql";';
-echo "${color_azul}Update your magento2/app/etc/env.php with the wright MYSQL IP${color_empty}";
-echo "${color_azul}Run:${color_empty}";
-echo 'docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; chmod +x bin/magento; bin/magento setup:upgrade;";';
-echo "${color_azul}Run:${color_empty}";
-echo 'docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; set-permissions";';
-echo "${color_azul}========================================Done=======================================${color_empty}";
+# echo "${color_azul}========================================Warning=======================================${color_empty}";
+# echo "${color_azul}If you get any SQL error, the reason should be wrong MYSQL docker IP, the right IP is:${color_empty}";
+# docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' docker-magento2_db_1;
+# echo "${color_azul}Run the next command with the correct IP:${color_empty}";
+# echo 'docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html/mysql_dumps/; mysql -u magento -pmagento -h 172.18.0.3 magento < magento.sql";';
+# echo "${color_azul}Update your magento2/app/etc/env.php with the wright MYSQL IP${color_empty}";
+# echo "${color_azul}Run:${color_empty}";
+# echo 'docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; chmod +x bin/magento; bin/magento setup:upgrade;";';
+# echo "${color_azul}Run:${color_empty}";
+# echo 'docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; set-permissions";';
+# echo "${color_azul}========================================Done=======================================${color_empty}";
 
 echo "${color_green}Done! You can access the magento project on: http://local.magento.com/${color_empty}";
 
