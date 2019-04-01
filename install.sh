6e2acd2c89c06e4093f70a0e9c754a6db3778488
Pedro Lima <phgdl.19@gmail.com>
2019-03-31 22:34:11 -0300

    Updating layout

--- install.sh
+++ install.sh
@@ -35,33 +35,27 @@ cp ${project_dir}/magento2/app/etc/env.php.dev ${project_dir}/magento2/app/etc/e
 cd ${project_dir}/docker-magento2;
 echo "${color_green}Installing Docker Environment${color_empty}";
 sudo docker-compose up -d --build;
+
 echo "${color_green}Populating DB${color_empty}";
-sudo docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html/mysql_dumps/; mysql -u magento -pmagento -h 172.22.0.110 magento < magento.sql";
+sudo docker exec -ti docker-magento2_web_1 sh -c "mysql -u magento -pmagento -h 172.22.0.110 magento < cd /var/www/html/mysql_dumps/magento.sql";
+
 echo "${color_green}Installing missing php extension${color_empty}";
 sudo docker exec -ti docker-magento2_web_1 sh -c "docker-php-ext-install bcmath;";
+
 echo "${color_green}Restarting apache${color_empty}";
 sudo docker exec -ti docker-magento2_web_1 sh -c "/etc/init.d/apache2 reload";
+
 echo "${color_green}Composer install${color_empty}";
 sudo docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; composer install;";
+
 echo "${color_green}Magento setup:upgrade${color_empty}";
 sudo docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; bin/magento setup:upgrade;";
+
 echo "${color_green}Fixing Permissions${color_empty}";
 sudo docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; set-permissions";
 
 echo "${color_green}Add docker apache IP to hosts${color_empty}";
 sudo -- sh -c "echo '172.22.0.105 local.magento.com' >> /etc/hosts";
-
-# echo "${color_azul}========================================Warning=======================================${color_empty}";
-# echo "${color_azul}If you get any SQL error, the reason should be wrong MYSQL docker IP, the right IP is:${color_empty}";
-# sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' docker-magento2_db_1;
-# echo "${color_azul}Run the next command with the correct IP:${color_empty}";
-# echo 'sudo docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html/mysql_dumps/; mysql -u magento -pmagento -h 172.22.0.110 magento < magento.sql";';
-# echo "${color_azul}Update your magento2/app/etc/env.php with the wright MYSQL IP${color_empty}";
-# echo "${color_azul}Run:${color_empty}";
-# echo 'sudo docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; chmod +x bin/magento; bin/magento setup:upgrade;";';
-# echo "${color_azul}Run:${color_empty}";
-# echo 'sudo docker exec -ti docker-magento2_web_1 sh -c "cd /var/www/html; set-permissions";';
-# echo "${color_azul}========================================Done=======================================${color_empty}";
 
 echo "${color_green}Done! You can access the magento project on: http://local.magento.com/${color_empty}";
 
